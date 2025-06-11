d1797e9243203db9ae3ba007220a902cf48c25d4
Louis <staletlouis@gmail.com>
2025-06-11 11:12:35 -0400

    data prep done

---
+++ Data_preparation.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+df = pd.read_csv('DATA_Gutenberg_Books_Summaries.csv')
+
+def get_data(df = df, word_count_range_start = 500, word_count_range_end = 750, number_of_books = 3):
+    df['summary'] = df['summary'].str.replace('__NEWL__', ' ')
+    filtered_df = df[(df['word_count'] >= word_count_range_start) & 
+                     (df['word_count'] <= word_count_range_end)].copy()
+    
+    filtered_df['word_count_csv'] = filtered_df['word_count']
+    
+    filtered_df['real_word_count'] = filtered_df['summary'].str.split().str.len()
+    result_df = filtered_df[['title', 'full_text', 'summary', 'word_count_csv', 'real_word_count']].head(number_of_books)
+    
+    return result_df
+
+
+
+
+
+
+
+
+
+
